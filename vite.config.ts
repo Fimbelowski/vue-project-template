55e837d8e5f49c7055c8931158141af3e4c7fda8
Christopher <ChrFimbel@gmail.com>
2022-05-02 18:59:02 -0600

    Added vitest.

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import * as path from 'path';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -9,5 +11,8 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src')
     },
+  },
+  test: {
+    environment: 'happy-dom',
   },
 })
