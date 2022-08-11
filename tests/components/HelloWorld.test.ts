ade164c203d35e582f0c3583521d87a5373e585f
Christopher <ChrFimbel@gmail.com>
2022-08-10 18:45:00 -0600

    Add tests for HelloWorld.vue.

---
+++ tests/components/HelloWorld.test.ts
@@ -0,0 +1,36 @@
+import { mount } from '@vue/test-utils';
+import { describe, expect, it, beforeEach, afterEach } from 'vitest';
+import HelloWorld from '../../src/components/HelloWorld.vue';
+
+describe('HellowWorld.vue', () => {
+  const props = {
+    msg: 'Lorem ipsum...',
+  };
+
+  let wrapper = mount(HelloWorld, { props });
+
+  beforeEach(() => {
+    wrapper = mount(HelloWorld, { props });
+  });
+
+  afterEach(() => {
+    wrapper.unmount();
+  });
+
+  it('should render a h1 with text equal to the "msg" prop', () => {
+    const h1 = wrapper.get('h1');
+
+    expect(h1.text()).toBe(props.msg);
+  });
+
+  it('should render a button whose initial text is "count is 0" that increments every time the button is clicked', async () => {
+    const button = wrapper.get('button');
+    const initialText = button.text();
+
+    await button.trigger('click');
+    const textAfterClick = button.text();
+
+    expect(initialText).toBe('count is 0');
+    expect(textAfterClick).toBe('count is 1');
+  });
+});
