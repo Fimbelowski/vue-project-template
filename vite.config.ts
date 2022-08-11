5b972ef8eea07ef4148dfdc7c1cb377f739b4d56
Christopher <ChrFimbel@gmail.com>
2022-08-10 18:24:51 -0600

    Re-initialize project with npm create vite.

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
-/// <reference types="vitest" />
-
-import * as path from 'path';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    },
-  },
-  test: {
-    environment: 'happy-dom',
-  },
+  plugins: [vue()]
 })
