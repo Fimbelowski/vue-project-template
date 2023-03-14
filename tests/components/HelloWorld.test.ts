11145ab47bae1994030db478140d18832e228d73
Christopher <ChrFimbel@gmail.com>
2023-03-14 15:03:59 -0600

    Run "npx prettier --write ."

--- tests/components/HelloWorld.test.ts
+++ tests/components/HelloWorld.test.ts
@@ -1,17 +1,11 @@
-import {
-  afterEach,
-  beforeEach,
-  describe,
-  expect,
-  it,
-} from 'vitest';
-import { mount } from '@vue/test-utils';
+import { afterEach, beforeEach, describe, expect, it } from "vitest";
+import { mount } from "@vue/test-utils";
 
-import HelloWorld from '@/components/HelloWorld.vue';
+import HelloWorld from "@/components/HelloWorld.vue";
 
-describe('HellowWorld.vue', () => {
+describe("HellowWorld.vue", () => {
   const props = {
-    msg: 'Lorem ipsum...',
+    msg: "Lorem ipsum...",
   };
 
   let wrapper = mount(HelloWorld, { props });
@@ -25,19 +19,19 @@ describe('HellowWorld.vue', () => {
   });
 
   it('should render a h1 with text equal to the "msg" prop', () => {
-    const h1 = wrapper.get('h1');
+    const h1 = wrapper.get("h1");
 
     expect(h1.text()).toBe(props.msg);
   });
 
   it('should render a button whose initial text is "count is 0" that increments every time the button is clicked', async () => {
-    const button = wrapper.get('button');
+    const button = wrapper.get("button");
     const initialText = button.text();
 
-    await button.trigger('click');
+    await button.trigger("click");
     const textAfterClick = button.text();
 
-    expect(initialText).toBe('count is 0');
-    expect(textAfterClick).toBe('count is 1');
+    expect(initialText).toBe("count is 0");
+    expect(textAfterClick).toBe("count is 1");
   });
 });
